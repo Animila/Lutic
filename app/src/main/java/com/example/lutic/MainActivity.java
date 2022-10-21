2359604ec1df58c5ec4419c2daee239183fe798c
animila <hristoforovila5@gmail.com>
2022-10-22 00:20:54 +0900

    Загрузочный экран

--- app/src/main/java/com/example/lutic/MainActivity.java
+++ app/src/main/java/com/example/lutic/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.lutic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button authButton, registerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        authButton = (Button) findViewById(R.id.authButton);
+        registerButton = (Button) findViewById(R.id.registerButton);
     }
 }
