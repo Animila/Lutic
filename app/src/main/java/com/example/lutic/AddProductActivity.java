ba5144e7737dd4855805b578d4893041b34a085f
animila <hristoforovila5@gmail.com>
2022-10-26 01:52:21 +0900

    Реализация добавления товара

--- app/src/main/java/com/example/lutic/AddProductActivity.java
+++ app/src/main/java/com/example/lutic/AddProductActivity.java
@@ -1,26 +1,184 @@
 package com.example.lutic;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
 
 public class AddProductActivity extends AppCompatActivity {
 
+    private String Description, Price, Name, SaveCurrentDate, SaveCurrentTime, productRandomKey;
+    private String downloadImageUrl;
     private ImageView productImage;
     private EditText productName, productAbout, productPrice;
-    private Button addNewProduct;
+    private Button AddNewProductBtn;
+    private static final int GalleryPick = 1;
+    private Uri ImageUri;
+    private StorageReference ProductImageRef;
+    private DatabaseReference ProductRef;
+    private ProgressDialog loadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_product);
+        Init();
+        
+        productImage.setOnClickListener(view -> {
+            OpenGallery();
+        });
+
+        AddNewProductBtn.setOnClickListener(view -> {
+            ValidateProductData();
+        });
+    }
+
+    public void Init() {
         productImage = (ImageView) findViewById(R.id.select_product_image);
-//        productName = (EditText) findViewById(R.id.product_name);
-//        productAbout = (EditText) findViewById(R.id.product_about);
-//        productPrice = (EditText) findViewById(R.id.product_price);
-//        addNewProduct = (Button) findViewById(R.id.add_product_save);
+        productName = (EditText) findViewById(R.id.productName);
+        productAbout = (EditText) findViewById(R.id.productAbout);
+        productPrice = (EditText) findViewById(R.id.productPrice);
+        AddNewProductBtn = (Button) findViewById(R.id.addProductSave);
+        ProductImageRef = FirebaseStorage.getInstance().getReference().child("Product_Images");
+        ProductRef = FirebaseDatabase.getInstance().getReference().child("Products ");
+        loadingBar = new ProgressDialog(this);
+    }
+
+    private void ValidateProductData() {
+        Description = productAbout.getText().toString();
+        Name = productAbout.getText().toString();
+        Price = productAbout.getText().toString();
+
+        if(ImageUri == null) {
+            Toast.makeText(this, "Добавьте изображение товара", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(Description)) {
+            Toast.makeText(this, "Добавьте описание товара", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(Name)) {
+            Toast.makeText(this, "Добавьте название товара", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(Price)) {
+            Toast.makeText(this, "Добавьте стоимость товара", Toast.LENGTH_SHORT).show();
+        } else {
+            StoreProductInformation();
+        }
+    }
+
+    private void StoreProductInformation() {
+
+        loadingBar.setTitle("Добавления товара");
+        loadingBar.setMessage("Ждите");
+        loadingBar.setCanceledOnTouchOutside(false);
+        loadingBar.show();
+
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat currentDate = new SimpleDateFormat("dd.MM.yyyy");
+        SaveCurrentDate = currentDate.format(calendar.getTime());
+        SimpleDateFormat currentTime = new SimpleDateFormat("HH.mm.ss");
+        SaveCurrentTime = currentTime.format(calendar.getTime());
+
+        productRandomKey = SaveCurrentDate + SaveCurrentTime;
+
+        StorageReference filePath = ProductImageRef.child(ImageUri.getLastPathSegment() + productRandomKey + ".webp");
+        final UploadTask uploadTask = filePath.putFile(ImageUri);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                String message = e.toString();
+                Toast.makeText(AddProductActivity.this, "Ошибка: "+message, Toast.LENGTH_SHORT).show();
+                loadingBar.dismiss();
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(AddProductActivity.this, "Изображение успешно загружено", Toast.LENGTH_SHORT).show();
+                Task<Uri> uriTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                    @Override
+                    public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                        if (!task.isSuccessful()) {
+                            throw task.getException();
+                        }
+                        downloadImageUrl = filePath.getDownloadUrl().toString();
+                        return filePath.getDownloadUrl();
+                    }
+                }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Uri> task) {
+                        if(task.isSuccessful()) {
+                            Toast.makeText(AddProductActivity.this, "Фото сохранено", Toast.LENGTH_SHORT).show();
+
+                            SaveProductInfoToDatabase();
+                        }
+                    }
+                });
+            }
+        });
+    }
+    private void SaveProductInfoToDatabase() {
+        HashMap<String, Object> productMap = new HashMap<>();
+        productMap.put("pid", productRandomKey);
+        productMap.put("date", SaveCurrentDate);
+        productMap.put("time", SaveCurrentTime);
+        productMap.put("description", Description);
+        productMap.put("image", downloadImageUrl);
+        productMap.put("price", Price);
+        productMap.put("name", Name);
+
+        ProductRef.child(productRandomKey).updateChildren(productMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()) {
+                    loadingBar.dismiss();
+                    Toast.makeText(AddProductActivity.this, "Товар добавлен", Toast.LENGTH_SHORT).show();
+                    Intent AddProductIntent = new Intent(AddProductActivity.this, SellerActivity.class);
+                    startActivity(AddProductIntent);
+                } else {
+                    String message = task.getException().toString();
+                    Toast.makeText(AddProductActivity.this, "Ошибка: "+message, Toast.LENGTH_SHORT).show();
+                    loadingBar.dismiss();
+                }
+            }
+        });
+    }
+
+    private void OpenGallery() {
+        Intent galleryIntent = new Intent();
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent,GalleryPick);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == GalleryPick && resultCode == RESULT_OK && data != null) {
+            ImageUri = data.getData();
+            productImage.setImageURI(ImageUri);
+        }
     }
 }
