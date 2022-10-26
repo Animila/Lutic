2609311e89a328e31109c85d99877bfb9c9650b6
animila <hristoforovila5@gmail.com>
2022-10-26 22:51:05 +0900

    Начало интерфейса пользователя

--- app/src/main/java/com/example/lutic/UserActivity.java
+++ app/src/main/java/com/example/lutic/UserActivity.java
@@ -1,28 +1,99 @@
 package com.example.lutic;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Menu;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.ui.AppBarConfiguration;
+
+import com.example.lutic.databinding.ActivityUserBinding;
 
 import io.paperdb.Paper;
 
-public class UserActivity extends AppCompatActivity {
-    private Button logoutBtnUser;
+public class UserActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    private AppBarConfiguration mAppBarConfiguration;
+    private ImageView openNav;
+
+    @SuppressLint("RtlHardcoded")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
 
-        logoutBtnUser = (Button) findViewById(R.id.logoutBtn);
+        Toolbar toolbar = findViewById(R.id.appBarUsers);
+        toolbar.setTitle(" ");
+        setSupportActionBar(toolbar);
 
-        logoutBtnUser.setOnClickListener(view -> {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toogle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                super.onDrawerSlide(drawerView, slideOffset);
+            }
+        };
+
+        drawer.addDrawerListener(toogle);
+        toogle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener((NavigationView.OnNavigationItemSelectedListener) this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.user, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.nav_orders) {
+
+        } else if(id == R.id.nav_products) {
+
+        } else if(id == R.id.nav_setting) {
+
+        } else if(id == R.id.nav_profile) {
+
+        } else if(id == R.id.nav_exit) {
             Paper.book().destroy();
-            Intent logoutIntent = new Intent(UserActivity.this, MainActivity.class);
-            startActivity(logoutIntent);
-        });
-
+            Intent loginIntent = new Intent(UserActivity.this, LoginActivity.class);
+            startActivity(loginIntent);
+        }
+        DrawerLayout drawerLayout= findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return false;
     }
 }
